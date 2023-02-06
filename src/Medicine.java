2e0d9d638bcc8f12a6cea6acdb27b725d5e74852
SATodorov18 <SATodorov18@codingburgas.bg>
2023-02-06 11:51:05 +0200

    commit

---
+++ src/Medicine.java
@@ -0,0 +1,46 @@
+public class Medicine {
+    private String name;
+    private double price;
+    private String expirationDate;
+
+    public Medicine(String name, double price, String expirationDate) {
+        this.name = name;
+        this.price = price;
+        this.expirationDate = expirationDate;
+    }
+
+    public Medicine(){
+        this.name="";
+        this.price=0.0;
+        this.expirationDate="";
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getExpirationDate() {
+        return expirationDate;
+    }
+
+    public void setExpirationDate(String expirationDate) {
+        this.expirationDate = expirationDate;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
